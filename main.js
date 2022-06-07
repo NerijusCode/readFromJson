22c30cc7c84d44a976fda56d489692c39c1f1a06
Torija <torija@gmail.com>
2022-06-07 22:00:35 +0300

    antros dienos darbai

--- main.js
+++ main.js
@@ -1,31 +1,175 @@
 'use strict';
 
-import { getUsers } from './src/users.js';
+import { getUsers, addUser } from './src/users.js';
+import { getCountry } from './src/countries.js';
 
 const tableBodyElement = document.querySelector('tbody');
+const addUsersForm = document.querySelector('#addUsers form');
+const searchResultsElement = document.querySelector('#searchResults');
+const searchUsersForm = document.querySelector('#searchUsers form');
+const sortBySurname = document.querySelector('#sortBySurname');
+const sortByName = document.querySelector('#sortByName');
+
 let users = [];
+
+addUsersForm.addEventListener('submit', addNewElement);
+searchUsersForm.addEventListener('submit', searchUsers);
+sortBySurname.addEventListener('click', () => {
+  sortBy('surname');
+  arrangeData();
+});
+
+sortByName.addEventListener('click', () => {
+  sortBy('name');
+  arrangeData();
+});
 
 initDocument();
 
+// function initDocument() {
+//   getUsers().then((data) => {
+//     users = data;
+//     arrangeData();
+//     appendFoot();
+//   });
+// }
+
+//initDoc perrašytas, įtraukiant duomenų sumappinimą nuskaitant userius
 function initDocument() {
   getUsers().then((data) => {
-      users = data;
-      arrangeData();  
+    let promises = data.map((user) =>
+      getCountry(user.country).then((country) => {
+        user.countryData = country[0];
+        return user;
+      })
+    );
+    Promise.all(promises).then((result) => {
+      users = result;
+      arrangeData();
+      appendFoot();
+    });
   });
 }
 
+// function arrangeData() {
+//   let usersElements = '';
+//   users.forEach((user) => {
+//     if (user.country) {
+//       getCountry(user.country)
+//         .then((country) => {
+//           usersElements += `<tr>
+//         <td>${user.id}</td>
+//         <td>${user.name}</td>
+//         <td>${user.surname}</td>
+//         <td><img src="${country[0].flags.png}" alt="${
+//             country[0].flag
+//           }" width="30"> ${user.country}, ${country[0].capital}</td>
+//         <td>${new Date(user.added).toLocaleDateString()}</td>
+//     </tr>`;
+//         })
+//         .then(() => (tableBodyElement.innerHTML = usersElements));
+//     }
+//   });
+// }
+
+//arrangeData perrašytas, naudojant jau sumappintus userius
 function arrangeData() {
   let usersElements = '';
   users.forEach((user) => {
-    usersElements += `<tr>
-                  <td>${user.id}</td>
-                  <td>${user.name}</td>
-                  <td>${user.surname}</td>
-                  <td>${user.city}</td>
-                  <td>${(new Date(user.added)).toLocaleDateString()}</td>
-              </tr>  
-          `;
+    if (user.country) {
+      usersElements += `<tr>
+          <td>${user.id}</td>
+          <td>${user.name}</td>
+          <td>${user.surname}</td>
+          <td><img src="${user.countryData.flags.png}" alt="${
+        user.countryData.flag
+      }" width="30"> ${user.country}, ${user.countryData.capital}</td>
+          <td>${new Date(user.added).toLocaleDateString()}</td>
+      </tr>`;
+      tableBodyElement.innerHTML = usersElements;
+    }
+  });
+}
+
+function addNewElement(event) {
+  event.preventDefault();
+  const name = addUsersForm.querySelector('#vardas');
+  const surname = addUsersForm.querySelector('#pavarde');
+  const country = addUsersForm.querySelector('#valstybe');
+  const newId = getNewId();
+  const newDate = new Date();
+
+  const user = {
+    id: newId,
+    name: name.value,
+    surname: surname.value,
+    country: country.value,
+    added: newDate,
+  };
+
+  addUser(user).then(() => {
+    initDocument();
   });
 
-  tableBodyElement.innerHTML = usersElements;
+  name.value = '';
+  surname.value = '';
+  country.value = '';
+}
+
+function getNewId() {
+  const idsArray = users.map((user) => user.id);
+  return Math.max(...idsArray) + 1;
+}
+
+function appendFoot() {
+  const totalUsers = users.length;
+  const tableFooterElement = document.querySelector('tfoot');
+  const footElement = `<tr>
+    <td colspan="2"><b>Iš viso vartotojų:</b></td>
+    <td colspan="3">${totalUsers}</td>
+  </tr>`;
+  tableFooterElement.innerHTML = footElement;
+}
+
+function searchUsers(event) {
+  event.preventDefault();
+  searchResultsElement.value = '';
+  const searchValue = searchUsersForm.querySelector('#paieska');
+  if (searchValue.value) {
+    const value = searchValue.value;
+    const valueInBold = `<b>${value}</b>`;
+    let searchResult = users.filter(
+      (user) => user.name.includes(value) || user.surname.includes(value)
+    );
+    let searchElement = '';
+    if (searchResult.length > 0) {
+      searchElement += `<div><span>Rezultatai:</span></div>`;
+      searchResult.forEach(
+        (item) =>
+          (searchElement += `<div>${item.id} ${item.name.replace(
+            value,
+            valueInBold
+          )} ${item.surname.replace(value, valueInBold)} ${
+            item.country
+          } ${new Date(item.added).toLocaleDateString()}</div>`)
+      );
+    } else {
+      searchElement = `<div>Rezultatų nerasta</div>`;
+    }
+    searchResultsElement.innerHTML = searchElement;
+    searchValue.value = '';
+  }
+}
+
+function sortBy(value) {
+  users.sort(compare);
+  function compare(a, b) {
+    if (a[value] < b[value]) {
+      return -1;
+    }
+    if (a[value] > b[value]) {
+      return 1;
+    }
+    return 0;
+  }
 }
--- src/users.js
+++ src/users.js
@@ -1,9 +1,25 @@
 'use strict';
 
-const usersData = '../data/users.json';
+// const usersData = '../data/users.json';
+const usersData = 'https://furtive-resisted-toque.glitch.me/users';
 
 export const getUsers = async () =>
   await fetch(usersData).then((response) => {
+    if (!response.ok) {
+      throw new Error('Įvyko klaida: ' + response.status);
+    }
+    return response.json();
+  });
+
+export const addUser = async (data) =>
+  await fetch(usersData, {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+  }).then((response) => {
     if (!response.ok) {
       throw new Error('Įvyko klaida: ' + response.status);
     }
